c4e6a61807a2d84e9dd24f8dc9b5a6c3932eb38e
lavertyaf <lavertyaileen@gmail.com>
2018-04-09 17:19:21 +0100

    repair to git

---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class WaterBottleTest {
+    private WaterBottle myWaterBottle;
+
+    @Before
+    public void before(){
+        myWaterBottle = new WaterBottle();
+    }
+@Test
+}
