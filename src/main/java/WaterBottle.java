9892ffc0ce7c93a48ed861c5d227afc8336c8215
lavertyaf <lavertyaileen@gmail.com>
2018-04-09 18:04:10 +0100

    water bottle complete

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,4 +1,26 @@
 public class WaterBottle {
 
+    private int volume;
+
+    public WaterBottle(int volume){
+        this.volume = volume;
+    }
+
+    public int getVolume(){
+        return this.volume;
+    }
+
+    public void decreaseVolume(){
+        this.volume -= 10;
+    }
+
+    public void emptyBottle(){
+        this.volume = 0;
+    }
+
+    public void refillBottle(){
+        this.volume = 100;
+    }
+
 
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,12 +1,44 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
     private WaterBottle myWaterBottle;
 
     @Before
     public void before(){
-        myWaterBottle = new WaterBottle();
+        myWaterBottle = new WaterBottle( 100);
     }
-@Test
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100, myWaterBottle.getVolume());
+    }
+
+    @Test
+    public void volumeGoesDown(){
+        myWaterBottle.decreaseVolume();
+        assertEquals(90, myWaterBottle.getVolume());
+    }
+
+    @Test
+    public void volumeGoesDownTwice(){
+        myWaterBottle.decreaseVolume();
+        myWaterBottle.decreaseVolume();
+        assertEquals(80, myWaterBottle.getVolume());
+    }
+
+    @Test
+    public void bottleIsEmpty(){
+        myWaterBottle.emptyBottle();
+        assertEquals(0, myWaterBottle.getVolume());
+    }
+
+    @Test
+    public void bottleIsRefilled(){
+        myWaterBottle.refillBottle();
+        assertEquals(100, myWaterBottle.getVolume());
+    }
+
 }
